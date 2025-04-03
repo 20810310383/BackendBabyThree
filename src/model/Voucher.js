36a3d37c70bc3a38bbed3e7d5afcef5e8200c328
Do Khac Tu <dokhactu1822@gmail.com>
2025-04-03 22:36:32 +0700

    first commit

---
+++ src/model/Voucher.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Voucher_Schema = new mongoose.Schema({   
+        code: { type: String},
+        dieuKien: { type: String},             
+        giamGia: { type: String},        
+        thoiGianHetHan: { type: String}, 
+        moTa:  { type: String},
+    },
+    { 
+        timestamps: true,   // createAt, updateAt
+    }
+);
+
+
+module.exports = mongoose.model("Voucher", Voucher_Schema);
